e86da7a9366ff6158695f42cf13a9706d3b3262a
Chengguang li <clii0084@student.monash.edu>
2021-04-25 17:25:47 +1000

    update regulation page title

--- component/RegulationHero.js
+++ component/RegulationHero.js
@@ -4,9 +4,11 @@ export const RegulationHero = () => {
     return (
         <div className={styles.regulationHero}>
             <p>
-            <div className={styles.regulationHeroInfo}>
-              <h1> Learn About The Regulations </h1>                           
-            </div>
+            {/* <div className={styles.regulationHeroInfo}> */}
+             <div className={styles.regulationHeroInfo1}> 
+               Learn About The Regulations 
+              </div>                        
+            {/* s */}
             <div className={styles.regulationHeroInfo}>
                 There are some regulations about eradicating activity you need to know                          
             </div>
