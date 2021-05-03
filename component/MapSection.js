56e148cf736c56b30d87dcb66395d777959e8cb2
Chengguang li <clii0084@student.monash.edu>
2021-05-03 23:09:28 +1000

    update svg

--- component/MapSection.js
+++ component/MapSection.js
@@ -60,7 +60,7 @@ export const MapSection = (props) => {
             latitude={ani.lat}
             longitude={ani.lon}
           >
-               <img src="/cat.png" alt="icon" width="30px" height="30px"></img>
+               <img src="/rabbit.svg" alt="icon" width="50px" height="50px"></img>
           </Marker>
         ))}
 
