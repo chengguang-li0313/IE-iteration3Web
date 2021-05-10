d235114686a536ba5e57bbfa661564843f2e983e
Vincent LAM <dev@dlops.ai>
2021-05-10 15:45:29 +0800

    fixed hero

--- component/AnimalHero.js
+++ component/AnimalHero.js
@@ -4,21 +4,7 @@ import { Nav } from '../component/Nav'
 export const AnimalHero = () => {
     return ( 
         <div className={styles.regulationHero}>
-<<<<<<< HEAD
- 
-=======
-             <Nav></Nav>
-            <p>
-            {/* <div className={styles.regulationHeroInfo}> */}
-             <div className={styles.regulationHeroInfo1}> 
-                Established Pest Animals In VIC
-              </div>                        
-            {/* s */}
-            <div className={styles.regulationHeroInfo}>
-                Certain Animals are declared as Pest Animals                         
-            </div>
-            </p>   
->>>>>>> 420dd1ed063276127e9508298d87b43da53ded98
+            <Nav></Nav>
         </div>
     )
 }
