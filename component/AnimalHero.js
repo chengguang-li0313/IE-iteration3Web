05e2185a177a463ad543f956454cbe66684c3b15
Chengguang li <clii0084@student.monash.edu>
2021-05-10 19:38:16 +1000

    hero section change back

--- component/AnimalHero.js
+++ component/AnimalHero.js
@@ -5,6 +5,16 @@ export const AnimalHero = () => {
     return ( 
         <div className={styles.regulationHero}>
             <Nav></Nav>
+            <p>
+            {/* <div className={styles.regulationHeroInfo}> */}
+             <div className={styles.regulationHeroInfo1}> 
+                Established Pest Animals In VIC
+              </div>                        
+            {/* s */}
+            <div className={styles.regulationHeroInfo}>
+                Certain Animals are declared as Pest Animals                         
+            </div>
+            </p>  
         </div>
     )
 }
