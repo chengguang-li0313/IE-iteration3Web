1fa361d7bbf0439f353ee9863591f16a7c20229e
Vincent LAM <dev@dlops.ai>
2021-05-12 19:34:58 +0800

    implemented about arrow tab

---
+++ component/AboutArrowTab.js
@@ -0,0 +1,75 @@
+import styles from '../styles/ArrowTab.module.css';
+import { useState } from 'react';
+
+export const AboutArrowTab = () => {
+    const [active, setActive] = useState('about');
+
+    const handleClick = (e) => {
+        setActive(e.target.getAttribute('name'))
+    }
+
+    return (
+      <div className={styles.arrowTabContainer}>
+        <h1>Our Vision</h1>
+        <div className={styles.arrowTabs}>
+          <div
+            name="about"
+            className={`${styles.arrowTab} ${
+              active === "about" ? styles.active : styles.empty
+            }`}
+            onClick={handleClick}
+          >
+            About Us
+          </div>
+          <div
+            name="vision"
+            className={`${styles.arrowTab} ${
+              active === "vision" ? styles.active : styles.empty
+            }`}
+            onClick={handleClick}
+          >
+            Vision
+          </div>
+          <div
+            name="mission"
+            className={`${styles.arrowTab} ${
+              active === "mission" ? styles.active : styles.empty
+            }`}
+            onClick={handleClick}
+          >
+            Mission
+          </div>
+          <div
+            name="values"
+            className={`${styles.arrowTab} ${
+              active === "values" ? styles.active : styles.empty
+            }`}
+            onClick={handleClick}
+          >
+            Values
+          </div>
+        </div>
+          <div className={`${
+              active === "about" ? styles.activePane : styles.tabPane
+            }`}>
+            We are a group of feral (non) enthusiasts and this is a joint initiative taken by us to help the lands of Farmers in Victoria being invaded by these species by providing information to identify and eradicate feral animals
+          </div>
+          <div className={`${
+              active === "vision" ? styles.activePane : styles.tabPane
+            }`}>
+            We help identify, prevent, control and eradicate pest animals from farmlands in Victoria in order to increase Agriculture Productivity 
+          </div>
+          <div className={`${
+              active === "mission" ? styles.activePane : styles.tabPane
+            }`}>
+            Raising awareness on the Impact of Invasive Animals on Agriculture by providing information on the declared established pest animals in Victori
+          </div>
+          <div className={`${
+              active === "values" ? styles.activePane : styles.tabPane
+            }`}>
+            Increase Farmer's Productivity, Promote Urban Wilderness, Keep Livestock and Native animals safe
+          </div>
+          
+      </div>
+    );
+}
--- pages/about.js
+++ pages/about.js
@@ -1,41 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/About.module.css'
-import { Layout } from '../component/Layout'
 import { Footer } from '../component/Footer' 
-import { Nav } from '../component/Nav'
+import { AboutArrowTab } from '../component/AboutArrowTab'
 import {AboutHero} from '../component/AboutHero'
 import ScrollToTop from "react-scroll-up";
 export default function Home() {
 
   return (
      <> 
-    {/* <Layout> */}
-    {/* <Nav></Nav> */}
     <AboutHero> </AboutHero>
-        <div className={styles.aboutContainer}>
-        
-            <img src='/logo.png'></img>
-            <h1>Our Vision</h1>
-            <p>We are a group of feral (non) enthusiasts and this is a joint initiative taken by us to help the lands of Farmers in Victoria being invaded by these species by providing information to identify and eradicate feral animals</p>
-            
-            <div className={styles.missionSection}>
-                <div className={styles.aboutCard}>
-                    <img src='/mission.png'></img>
-                    <h1>Mission</h1>
-                    <p>Raising awareness on the Impact of Invasive Animals on Agriculture by providing information on the declared established pest animals in Victoria</p>
-                </div>
-                <div className={styles.aboutCard}>
-                    <img src='/vision.jpeg'></img>
-                    <h1>Vision</h1>
-                    <p>We help identify, prevent, control and eradicate pest animals from farmlands in Victoria in order to increase Agriculture Productivity</p>
-                </div>
-                <div className={styles.aboutCard}>
-                    <img src='/values.png'></img>
-                    <h1>Values</h1>
-                    <p>Increase Farmer's Productivity, Promote Urban Wilderness, Keep Livestock and Native animals safe</p>
-                </div>
-            </div>
-        </div>
+    <AboutArrowTab></AboutArrowTab>
         <ScrollToTop 
     showUnder={160}
     // easing="easeInExpo"
@@ -43,9 +17,8 @@ export default function Home() {
     cursor="pointer"
      >    
     <img src="/upup.svg" width="30px" height="30px"/> 
-  </ScrollToTop> 
+        </ScrollToTop> 
         <Footer></Footer>
-    {/* </Layout> */}
     </>
   )
 }
