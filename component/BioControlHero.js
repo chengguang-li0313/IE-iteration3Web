33b325098e46a9c4f0c986c4a8ed1c70bd1e0fbb
Vincent LAM <dev@dlops.ai>
2021-05-17 18:39:36 +0800

    changed heros section in bio and conventional

--- component/BioControlHero.js
+++ component/BioControlHero.js
@@ -1,9 +1,21 @@
-import styles from "../styles/Control.module.css";
+import ControlStyles from "../styles/Control.module.css";
+import styles from '../styles/ControlHero.module.scss'
+
 
 export const BioControlHero = (props) => {
 
     return (
-        <div className={styles.bioControlHero}>
+        <div className={ControlStyles.bioControlHero}>
+            <p>
+            {/* <div className={styles.regulationHeroInfo}> */}
+             <div className={styles.regulationHeroInfo1}> 
+               Biological Control
+              </div>                        
+            {/* s */}
+            <div className={styles.regulationHeroInfo}>
+                Learn about biological control methods                       
+            </div>
+            </p>   
         </div>
     );
 };
--- component/ConventionalHero.js
+++ component/ConventionalHero.js
@@ -1,9 +1,21 @@
-import styles from "../styles/Control.module.css";
+import ControlStyles from "../styles/Control.module.css";
+import styles from '../styles/ControlHero.module.scss'
+
 
 export const ConventionalHero = (props) => {
 
     return (
-        <div className={styles.conventionalHero}>
+        <div className={ControlStyles.conventionalHero}>
+            <p>
+            {/* <div className={styles.regulationHeroInfo}> */}
+             <div className={styles.regulationHeroInfo1}> 
+               Conventional Control Methods
+              </div>                        
+            {/* s */}
+            <div className={styles.regulationHeroInfo}>
+                Learn about conventional control methods                     
+            </div>
+            </p>   
         </div>
     );
 };
