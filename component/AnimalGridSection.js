f8584991aa57f8b242c8d23669d9344a86e71dec
Vincent LAM <dev@dlops.ai>
2021-05-18 13:14:45 +0800

    changed bio attributes to be like impact card

--- component/AnimalGridSection.js
+++ component/AnimalGridSection.js
@@ -1,46 +1,64 @@
 import styles from "../styles/Animals.module.css";
 import { Accordion } from './Accordion'
 import { BioCard } from './BioCard'
+import Carousel from "react-multi-carousel";
+import { AnimalImpactCard } from './AnimalImpactCard'
 
 export const AnimalGridSection = (props) => {
   const { data } = props;
 
+  const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 1500 },
+        items: 3,
+    },
+    desktop: {
+        breakpoint: { max: 1500, min: 1024 },
+        items: 3,
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 3,
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+    },
+  };
+
   return (
     <Accordion title={'Biological Attributes'}>
-      <div className={styles.infoSection}>
-        <BioCard
-         title={"Appearance"}
-         desc={data.appearance}
-         img={data.appearanceImage}
-         >
-        </BioCard>
-
-        <BioCard
-          title={"Diet"}
-          desc={data.diet}
+       <Carousel 
+            responsive={responsive} 
+            showDots={true}
+            > 
+        <AnimalImpactCard
+          img={data.appearanceImage}
+          title={"Appearance"}
+          text={data.appearance}
+        ></AnimalImpactCard>
+        <AnimalImpactCard
           img={data.image1}
-        ></BioCard>
-
-        <BioCard
-          title={"Behaviour"}
-          desc={data.behaviour}
+          title={"Diet"}
+          text={data.diet}
+        ></AnimalImpactCard>
+        <AnimalImpactCard
           img={data.image2}
-        ></BioCard>
-
-        <BioCard
-          title={"Preffered Habitat"}
-          desc={data.habitat}
+          title={"Behaviour"}
+          text={data.behaviour}
+        ></AnimalImpactCard>
+        <AnimalImpactCard
           img={data.image3}
-
-        ></BioCard>
-
-        <BioCard
-          title={"Predation"}
-          desc={data.prey}
+          title={"Preffered Habitat"}
+          text={data.habitat}
+        ></AnimalImpactCard>
+        <AnimalImpactCard
           img={data.image4}
-
-        ></BioCard>
-      </div>
+          title={"Predation"}
+          text={data.prey}
+        ></AnimalImpactCard>
+        </Carousel>
     </Accordion>
   );
 };
